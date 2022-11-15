eba4f86014833fb6774fcffb149393c694adbd59
neobis2022team2autnm <project.autumn.2022.team2@gmail.com>
2022-11-15 21:50:23 +0600

    init : create new sections

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,13 @@ if (filter) {
     })
   })
 }
+
+//swaiper
+
+const swiper = new Swiper('.swiper', {
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
